ad4a3da251dedd2eb1664c2cd4de6082627ab78f
Noam Rubin <rubin.m.noam@gmail.com>
2013-04-21 16:27:57 -0400

    Added data and notes

---
+++ LabNotes.m
@@ -0,0 +1,20 @@
+%Lab9Notes
+
+% Experiment1
+% Exp1P1: V2 = 2V
+% load('Exp1P1.mat');
+% plot(V1,Vout,'.'); hold all;
+% Exp1P2: V2 = 3V
+% load('Exp1P2.mat');
+% plot(V1,Vout,'.');
+% Exp1P3: V2 = 4V
+% load('Exp1P3.mat');
+% plot(V1,Vout,'.');
+
+
+%Experiment2
+%Exp2P1: Sweep Vdm around V2 = 2.5V
+%Exp2P2: Sweep Vout while measuring Iout, Vcm = 2.5V, Vdm = 0
+%Exp2P3: Sweep Vdm around V2 = 2.5V, Vout = 2.5V
+
+
