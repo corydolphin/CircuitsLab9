14c97dd7b4a717b481391b205bf76c3e294cf23b
Noam Rubin <rubin.m.noam@gmail.com>
2013-04-21 17:06:01 -0400

    finished exp2 part 2

---
+++ Exp2p2.m
@@ -0,0 +1,13 @@
+%Exp2p2
+load('Data/Exp2P2.mat')
+%31 259
+coeffs = polyfit(Vout(31:259), Iout(31:259),1);
+plot(Vout,Iout,'o'); hold on;
+plot(Vout,Vout * coeffs(1) + coeffs(2),'-','LineWidth',2);
+xlim([0 5]);
+% ylim([-14e-4 2e-4]);
+xlabel('V_{out} (Volts)');
+ylabel('I_{out} (Amps)');
+title('I_{out} as a Function of V_{out} with V_{dm} = 0');
+legend('Data',strcat('Incremental Output Resistance R_{out} = ', num2str(1/coeffs(1)), ' \Omega'),'Location','SouthEast');
+rout = 1/coeffs(1);
